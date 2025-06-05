0216076dc545b7762a7395f80508284da8fa2aac
David Sent <david.sent@app-akademie.com>
2025-06-05 14:30:42 +0200

    recreate android dir

--- android/app/src/main/kotlin/com/example/simple_beautiful_checklist_exercise/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/simple_beautiful_checklist_exercise/MainActivity.kt
@@ -2,4 +2,4 @@ package com.example.simple_beautiful_checklist_exercise
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity : FlutterActivity()
